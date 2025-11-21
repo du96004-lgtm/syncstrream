88052da0e518641f446ef1cbb3ef0c057aaa43c4
Praveen Andavarapu <andavarapupraveen857@gmail.com>
2025-11-21 02:31:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3136805657-8f2d9",
+  "appId": "1:584869224104:web:a66e20ab24238b196a2c9d",
+  "apiKey": "AIzaSyBLeHyp_M0YOJWtULcy3qVW_HAqJNwNAaI",
+  "authDomain": "studio-3136805657-8f2d9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "584869224104"
+};
